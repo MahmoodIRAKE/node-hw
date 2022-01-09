a52c242c8c6cb6984dba4fe847af9014408a3209
MahmoodIRAKE <mhmod.eraqi@gmail.com>
2022-01-09 14:23:17 +0200

    first Ex

---
+++ node-vs-js.js
@@ -0,0 +1,17 @@
+// Write in your own words the following:
+// What are 3 things node.js has that the client side javascript doesn’t
+// have. And the reasons why.
+/*
+ 1)non blocking i/O system 
+2) it only runs on the v8 engine
+3) it gives us the ability to run js code outside the browser
+*/
+
+// What are 3 things the client side javascript has that node.js doesn’t
+// have. And the reasons why.
+
+/*
+1) manpulating the dom and adding html tags .
+2) it can run on alomst any engine 
+3) it only runs on the browser
+*/
